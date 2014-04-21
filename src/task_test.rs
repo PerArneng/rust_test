293ece9ed9be5e9baebc76a606eba5cc26b82010
Per Arneng <per.arneng@scalebit.com>
2014-04-21 15:53:29 +0200

    made the task test work

--- src/task_test.rs
+++ src/task_test.rs
@@ -1,23 +1,25 @@
 
 
-
-
 fn main() {
+
 	println!("task test");
 
 	let (tx, rx): (Sender<int>, Receiver<int>) = channel();
 
 
+	let tx1 = tx.clone();
 	spawn(proc() {
-    	tx.send(10);
+    	tx1.send(10);
 	});
 
+	let tx2 = tx.clone();
 	spawn(proc() {
-    	tx.send(20);
+    	tx2.send(20);
 	});
 
 	let a = rx.recv();
 	let b = rx.recv();
 
 	println!("a={} b={}", a, b);
+
 }
